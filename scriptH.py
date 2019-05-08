ec63cc419eb7781baa3fb9c43b30745d803d7f00
powerkyw59 <43169670+powerkyw59@users.noreply.github.com>
2019-05-09 00:02:21 +0900

    1일차
    끝내려면 한참 남았다...

--- scriptH.py
+++ scriptH.py
@@ -2,7 +2,52 @@ from tkinter import*
 
 
 
-class program:
-    def init(self):
+class mainState:
+    def goSearchState(self):
+        self.exit()
+        self.window.destroy()
+        searchState()
         pass
-    
+    def goBookMarkState(self):
+
+        pass
+    def __init__(self):
+
+        self.window=Tk()
+        self.window.geometry('300x300')
+        self.window.title('ShelterFinder')
+        Label(text="대피소",font=('Times New Roman',40)).pack()
+
+        b = Button(self.window, text="지역검색",command=self.goSearchState,font=('Times New Roman',15))
+        b.place(x=100,y=130)
+
+
+        d = Button(self.window, text="즐겨찾기", command=self.goBookMarkState, font=('Times New Roman', 15))
+        d.place(x=100, y=200)
+
+
+
+
+        mainloop()
+        print(12)
+    pass
+    def exit(self):
+       pass
+
+class searchState():
+    def __init__(self):
+        self.window=Tk()
+        self.window.title('SearchState')
+        self.window.geometry('700x500')
+        Label(text="대피소",font=('Times New Roman',40)).pack()
+
+
+
+        mainloop()
+        print(3)
+    pass
+
+
+
+
+mainState()
