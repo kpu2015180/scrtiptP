e5860e94bc1a7308961aefad3c50f779a9573858
powerkyw59 <43169670+powerkyw59@users.noreply.github.com>
2019-05-12 22:21:12 +0900

    search 한줄 제작
    제작함

--- searchState.py
+++ searchState.py
@@ -10,23 +10,28 @@ class SearchState():
         #Label(text="대피소",font=('Times New Roman',40)).pack()
         #mainloop()
         pass
-
+    def searchD(self):
+        pass
     def enter(self):
         self.window = Tk()
         self.window.title('SearchState')
         self.window.geometry('700x500')
 
-        C1=ttk.Combobox(self.window,width=15,height=30,values=name)
-        C1.place(x=30,y=50)
-        C1.set("지역")
+        c1=ttk.Combobox(self.window,width=15,height=30,values=name)
+        c1.place(x=70,y=50)
+        c1.set("시")
 
-        C1 = ttk.Combobox(self.window, width=15, height=30, values=name)
-        C1.place(x=30, y=50)
-        C1.set("지역")
+        c2 = ttk.Combobox(self.window, width=15, height=30, values=name)
+        c2.place(x=240, y=50)
+        c2.set("군")
 
-        C1 = ttk.Combobox(self.window, width=15, height=30, values=name)
-        C1.place(x=30, y=50)
-        C1.set("지역")
+        c3 = ttk.Combobox(self.window, width=15, height=30, values=name)
+        c3.place(x=410, y=50)
+        c3.set("구")
+
+        
+        Button(self.window,command=self.searchD,text="  검색  ").place(x=580,y=45)
+
         #Label(self.window,text="대피소", font=('Times New Roman', 40)).pack()
         mainloop()
     def exit(self):
