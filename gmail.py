c22a2e1e0233bedc49358ebd59f496aec4b26402
2015180012박건호 <ehdrpakt@naver.com>
2019-05-26 21:28:59 +0900

    메일보내기 밑준비

---
+++ gmail.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import mimetypes
+import mysmtplib
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+
+#global value
+host = "smtp.gmail.com" # Gmail STMP 서버 주소.
+port = "587"
+htmlFileName = "logo.html"
+
+senderAddr = "ehdrpakt@gmail.com"     # 보내는 사람 email 주소.
+recipientAddr = "ehdrpakt@naver.com"   # 받는 사람 email 주소.
+
+#msg = MIMEBase("multipart", "alternative")
+
+msg =MIMEText("한글테스트")
+msg['Subject'] = "Test email in Python 3.0" #제목
+msg['From'] = senderAddr
+msg['To'] = recipientAddr
+
+# MIME 문서를 생성합니다.
+htmlFD = open(htmlFileName, 'rb')
+HtmlPart = MIMEText(htmlFD.read(),'html', _charset = 'UTF-8' )
+htmlFD.close()
+
+# 만들었던 mime을 MIMEBase에 첨부 시킨다.
+#msg.attach(HtmlPart)
+
+# 메일을 발송한다.
+s = mysmtplib.MySMTP(host,port)
+#s.set_debuglevel(1)        # 디버깅이 필요할 경우 주석을 푼다.
+s.ehlo()
+s.starttls()
+s.ehlo()
+s.login("ehdrpakt@gmail.com","ssiber12")
+s.sendmail(senderAddr , [recipientAddr], msg.as_string())
+s.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
